e4c7f38ee02eeaf08a11a22ad61737814a71ce43
RitheshJgit <ritheshjayapal@gmail.com>
2025-02-15 16:59:54 +0530

    Adding Linked List

--- Generic/LinkedList/Main.java
+++ Generic/LinkedList/Main.java
@@ -3,10 +3,12 @@ package Generic.LinkedList;
 public class Main extends doubleyLinkedList{
     public static void main(String[] args) {
         doubleyLinkedList dl = new doubleyLinkedList();
-        dl.insertAtFirst(40);
         dl.insertAtFirst(30);
         dl.insertAtFirst(20);
         dl.insertAtFirst(10);
+        dl.display();
+        dl.insertBetween(1,25);
+        System.out.println();
         dl.display();
     }
 }
